f7badba895fbd4bc2d5f08c8c40ab8395d5baf80
mustapha1900 <mustapha.toubalseghir@gmail.com>
2024-04-07 17:08:47 -0400

    creation du code JS pour les page Index et contact avec du code

---
+++ Script/contact.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+const formulaire = document.querySelector("#contactForm")
+
+const nav = document.querySelectorAll ()
+
+
+formulaire.addEventListener ("mouseover",() => {
+    
+    formulaire.style.backgroundColor = "red"    
+})
+
+formulaire.addEventListener ("mouseleave",() => {
+    
+    formulaire.style.backgroundColor = ""    
+})
+
+
+
+
+
+
+})
