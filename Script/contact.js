82ae056a22728da2d96410d4e353079b7706c40f
mustapha1900 <mustapha.toubalseghir@gmail.com>
2024-04-08 00:29:38 -0400

    ajout du code JS sur Index Html

--- Script/contact.js
+++ Script/contact.js
@@ -2,18 +2,32 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 const formulaire = document.querySelector("#contactForm")
 
-const nav = document.querySelectorAll ()
+
 
 
 formulaire.addEventListener ("mouseover",() => {
     
-    formulaire.style.backgroundColor = "red"    
+    formulaire.style.backgroundColor = "orange"    
 })
 
 formulaire.addEventListener ("mouseleave",() => {
     
     formulaire.style.backgroundColor = ""    
 })
+
+const listItems = document.querySelectorAll('nav ul li');
+
+listItems.forEach(item => {
+    item.addEventListener('mouseenter', function () {
+        this.style.transform = 'scale(1.3)'; 
+       // this.style.backgroundColor = "grey";                
+    });
+
+    item.addEventListener('mouseleave', function () {
+        this.style.transform = 'scale(1)'; 
+        //this.style.backgroundColor= ""        
+    });
+});
 
 
 
